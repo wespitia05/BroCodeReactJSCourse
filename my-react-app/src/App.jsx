017f5dd122e731c54085ed68268ef02ead8c396b
william <espiw@farmingdale.edu>
2026-01-12 19:09:36 -0500

    Bro Code reactjs applied footer to our site

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
   return(
-    <Header></Header> // or <Header/>
+    // to return two separate imported components, enclose them in empty alligators
+    // <Header/> and <Footer/> both can work as well
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
   );
 }
 
