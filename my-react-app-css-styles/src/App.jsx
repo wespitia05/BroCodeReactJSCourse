14701be243f64eed9c549105987c8e7f6559fdb5
william <espiw@farmingdale.edu>
2026-01-13 23:31:34 -0500

    Bro Code reactjs css styling created button and imported it to app

--- my-react-app-css-styles/src/App.jsx
+++ my-react-app-css-styles/src/App.jsx
@@ -1,6 +1,18 @@
+// how to style react components with css
+// --------------------------------------
+// (not including external frameworks or preprocessors)
 
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from './Button.jsx'
 
 function App() {
+    return(
+      <Button></Button>
+    );
+
 }
 
 export default App
