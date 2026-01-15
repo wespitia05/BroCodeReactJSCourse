756e061a13bac6a36a79d04ef92aa821ed71c75d
william <espiw@farmingdale.edu>
2026-01-14 19:22:25 -0500

    Bro Code reactjs props did some slight styling for better readability

--- my-react-app-props/src/Students.jsx
+++ my-react-app-props/src/Students.jsx
@@ -1,6 +1,6 @@
 function Student(props) {
     return(
-        <div>
+        <div className="student">
             <p>Name: {props.name}</p>
             <p>Age: {props.age}</p>
             <p>Student: {props.isStudent ? "Yes" : "No"}</p>
