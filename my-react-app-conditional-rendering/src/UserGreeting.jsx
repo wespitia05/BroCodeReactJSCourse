f9b77ae47b5395c75ab8be3d1525bbe43b1b20f0
william <espiw@farmingdale.edu>
2026-01-15 18:41:43 -0500

    Bro Code reactjs cond rend not logged in display

--- my-react-app-conditional-rendering/src/UserGreeting.jsx
+++ my-react-app-conditional-rendering/src/UserGreeting.jsx
@@ -2,6 +2,11 @@ function UserGreeting(props) {
     if(props.isLoggedIn) {
         return <h2>Welcome {props.username}</h2>
     }
+    else {
+        return<h2>Please log in to continue</h2>
+    }
+    // could also do this
+    // return(props.isLoggedIn ? <h2>Welcome {props.username}</h2> : <h2>Please log in to continue</h2>);
 }
 
 export default UserGreeting
