6d21bd053e49753f50807a0020b7599f8fd84dd6
william <espiw@farmingdale.edu>
2026-01-15 18:46:51 -0500

    Bro Code reactjs cond rend css for logged in vs not logged in

--- my-react-app-conditional-rendering/src/UserGreeting.jsx
+++ my-react-app-conditional-rendering/src/UserGreeting.jsx
@@ -1,9 +1,9 @@
 function UserGreeting(props) {
     if(props.isLoggedIn) {
-        return <h2>Welcome {props.username}</h2>
+        return <h2 className="welcome-message">Welcome {props.username}</h2>
     }
     else {
-        return<h2>Please log in to continue</h2>
+        return<h2 className="login-prompt">Please log in to continue</h2>
     }
     // could also do this
     // return(props.isLoggedIn ? <h2>Welcome {props.username}</h2> : <h2>Please log in to continue</h2>);
