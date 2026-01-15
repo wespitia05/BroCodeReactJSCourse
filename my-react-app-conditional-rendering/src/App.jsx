64b6bc846f9c981a8b9d38955707cd7773900fc0
william <espiw@farmingdale.edu>
2026-01-14 19:48:59 -0500

    Bro Code reactjs cond rend setup

---
+++ my-react-app-conditional-rendering/src/App.jsx
@@ -0,0 +1,9 @@
+// conditional rendering = allows you to control what gets rendered
+//                         in your application based on certain conditions
+//                         (show, hide, or change components)
+
+function App() {
+
+}
+
+export default App
