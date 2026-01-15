af2a7639d08a15a43be5d017c43577c2fa6d23fb
william <espiw@farmingdale.edu>
2026-01-14 19:16:47 -0500

    Bro Code reactjs props using props to display some data

--- my-react-app-props/src/App.jsx
+++ my-react-app-props/src/App.jsx
@@ -1,6 +1,14 @@
-
+// props = read-only properties that are shared between components
+//         a parent component can send data to a child component
+//         <Component key=value />
+import Student from './Students.jsx'
 
 function App() {
+    return(
+        <>
+            <Student name="Spongebob" age={30} isStudent={true}/>
+        </>
+    );
 }
 
 export default App
