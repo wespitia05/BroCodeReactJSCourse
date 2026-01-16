62d6729ba745998817d9069d742895ad8ed11b09
william <espiw@farmingdale.edu>
2026-01-16 15:01:43 -0500

    Bro Code reactjs lists assigning key id for array of objects

--- my-react-app-lists/src/Lists.jsx
+++ my-react-app-lists/src/Lists.jsx
@@ -1,9 +1,19 @@
 function List() {
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    // const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const fruits = [{id: 1, name: "apple", calories: 95}, 
+                    {id: 2, name: "orange", calories: 45},
+                    {id: 3, name: "banana", calories: 105}, 
+                    {id: 4, name: "coconut", calories: 159}, 
+                    {id: 5, name: "pineapple", calories: 37}];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    // fruits.sort(); // sorts the fruits alphabetically
 
-    return(<ul>{listItems}</ul>);
+    // const listItems = fruits.map(fruit => <li>{fruit}</li>);
+
+    // need to assign a key id
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>);
+
+    return(<ul>{listItems}</ul>); // <ol> for numbered
 }
 
 export default List
