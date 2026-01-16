2d44277e51267695ade3d4a10b1391bbcd3cab5f
william <espiw@farmingdale.edu>
2026-01-16 15:06:09 -0500

    Bro Code reactjs lists sorting array of objects differently

--- my-react-app-lists/src/Lists.jsx
+++ my-react-app-lists/src/Lists.jsx
@@ -7,11 +7,18 @@ function List() {
                     {id: 5, name: "pineapple", calories: 37}];
 
     // fruits.sort(); // sorts the fruits alphabetically
+    fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL
+    fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSE ALPHABETICAL
+    fruits.sort((a, b) => a.calories - b.calories); // NUMERICAL ORDER
+    fruits.sort((a, b) => b.calories - a.calories); // REVERSE NUMERICAL ORDER
 
     // const listItems = fruits.map(fruit => <li>{fruit}</li>);
 
     // need to assign a key id
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp;
+                                            <b>{fruit.calories}</b>
+                                          </li>);
 
     return(<ul>{listItems}</ul>); // <ol> for numbered
 }
