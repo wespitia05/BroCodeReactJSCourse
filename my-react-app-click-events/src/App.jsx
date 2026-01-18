a18de66927781cc17ae321b79688754edad6e282
william <espiw@farmingdale.edu>
2026-01-18 11:13:27 -0500

    Bro Code reactjs click events created button file and imported it into app

--- my-react-app-click-events/src/App.jsx
+++ my-react-app-click-events/src/App.jsx
@@ -1,6 +1,13 @@
+// click event = an interaction when a user clicks on a specific element
+//               we can respond to clicks by passing 
+//               a callback to the onClick event handler
+
+import Button from './Button.jsx'
 
 function App() {
-    
+    return(
+        <Button />
+    );
 }
 
 export default App
