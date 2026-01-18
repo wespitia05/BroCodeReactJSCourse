8dce28943c3d568cc9aafa2cd47e826ec4f423f1
william <espiw@farmingdale.edu>
2026-01-18 11:21:39 -0500

    Bro Code reactjs click events using conditions for our button (count)

--- my-react-app-click-events/src/Button.jsx
+++ my-react-app-click-events/src/Button.jsx
@@ -1,12 +1,24 @@
 function Button() {
 
-    const handleClick = () => console.log("OUCH!");
+    // const handleClick = () => console.log("OUCH!");
 
-    const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+    // const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+
+    let count = 0;
+
+    const handleClick = (name) => {
+        if(count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`)
+        }
+        else {
+            console.log(`${name} stop clicking me!`)
+        }
+    }
 
     return(
         // using arrow function prevents from calling the function right away
-        <button onClick={() => handleClick2("william")}>Click Me 😄</button>
+        <button onClick={() => handleClick("william")}>Click Me 😄</button>
     );
 }
 
