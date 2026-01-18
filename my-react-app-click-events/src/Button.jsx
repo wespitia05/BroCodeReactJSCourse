0a30b6e7de70b715ff181cdce9b1354471d4056f
william <espiw@farmingdale.edu>
2026-01-18 11:17:43 -0500

    Bro Code reactjs click events button functions

--- my-react-app-click-events/src/Button.jsx
+++ my-react-app-click-events/src/Button.jsx
@@ -1,5 +1,13 @@
 function Button() {
 
+    const handleClick = () => console.log("OUCH!");
+
+    const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+
+    return(
+        // using arrow function prevents from calling the function right away
+        <button onClick={() => handleClick2("william")}>Click Me 😄</button>
+    );
 }
 
 export default Button
