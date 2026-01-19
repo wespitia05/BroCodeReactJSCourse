ede4e48f50bc1f6e8b5e7034daf82ec93bd2bf44
william <espiw@farmingdale.edu>
2026-01-19 17:52:35 -0500

    Bro Code reactjs useState some notes

--- my-react-app-use-state/src/App.jsx
+++ my-react-app-use-state/src/App.jsx
@@ -1,4 +1,10 @@
+// react hook = special function that allows functional components
+//              to use react features without writing class components (react v16.8)
+//              (useState, useEffect, useContext, useReducer, useCallback, and more...)
 
+// useState() = a react hook that allows the creation of statefull variable
+//              and a setter function to update its value in the virtual dom
+//              [name, setName]
 
 function App() {
 
