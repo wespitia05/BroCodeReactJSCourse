a03051f0436c3d8308c627dbb4dfc723d8933bc9
william <espiw@farmingdale.edu>
2026-01-20 17:26:12 -0500

    Bro Code reactjs useState counter returning hmtl elements successfully

--- my-react-app-use-state/src/Counter.jsx
+++ my-react-app-use-state/src/Counter.jsx
@@ -16,6 +16,15 @@ function Counter() {
     const reset = () => {
         setCount(0);
     }
+
+    return(
+        <div className="counter-container">
+            <p className="count-display">{count}</p>
+            <button className="counter-button" onClick={decrement}>Decrement</button>
+            <button className="counter-button" onClick={reset}>Reset</button>
+            <button className="counter-button" onClick={increment}>Increment</button>
+        </div>
+    );
 }
 
 export default Counter
