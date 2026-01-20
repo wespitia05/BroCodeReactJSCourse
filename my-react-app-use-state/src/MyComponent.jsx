e8733144375ed5714a2d02f61d85dcd533181401
william <espiw@farmingdale.edu>
2026-01-20 17:09:00 -0500

    Bro Code reactjs useState incrementing a number

--- my-react-app-use-state/src/MyComponent.jsx
+++ my-react-app-use-state/src/MyComponent.jsx
@@ -4,16 +4,26 @@ import React, {useState} from 'react';
 function MyComponent() {
     // set guest as the initial variable that will display before pressing button
     const [name, setName] = useState("Guest");
+    // set age 0 as the initial age
+    const [age, setAge] = useState(0);
 
     // when this function is called, we use the setName parameter to set the name
     const updateName = () => {
         setName("Spongebob");
     }
 
+    // when this function is called, we increment the age by 1
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
         </div>
     );
 }
