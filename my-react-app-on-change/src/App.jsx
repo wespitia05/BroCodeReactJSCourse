4878bc6fa26d6c979442e52d8185cec0ef3eb86d
william <espiw@farmingdale.edu>
2026-01-23 17:56:37 -0500

    Bro Code reactjs onChange quick notes before starting

--- my-react-app-on-change/src/App.jsx
+++ my-react-app-on-change/src/App.jsx
@@ -1,3 +1,6 @@
+// onChange = event handler used primarily with form elements
+//            ex. <input>, <textarea>, <select>, <radio>
+//            triggers a function every time the value of the input changes
 
 function App() {
 
