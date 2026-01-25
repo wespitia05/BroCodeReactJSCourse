fca026cbe4adfc204a3ffe7d79156c2201c02d16
william <espiw@farmingdale.edu>
2026-01-25 13:24:54 -0500

    Bro Code reactjs onChange using the textarea element

--- my-react-app-on-change/src/MyComponent.jsx
+++ my-react-app-on-change/src/MyComponent.jsx
@@ -3,17 +3,20 @@ import React, {useState} from "react";
 function MyComponent() {
     // creating stateful name variable w/ setter function
     // inital value is an empty string
-    const [name, setName] = useState("");
+    const [name, setName] = useState("Guest");
     const [quantity, setQuantity] = useState(0);
+    const [comment, setComment] = useState("");
 
     // this function will handle changing the name
     // invoke set name function, access the event object, access target and get value property
     function handleNameChange(event) {
         setName(event.target.value);
     }
-
     function handleQuantityChange(event) {
         setQuantity(event.target.value);
+    }
+    function handleCommentChange(event) {
+        setComment(event.target.value);
     }
 
     // onChange event handler triggers the function every time the input value changes
@@ -24,6 +27,9 @@ function MyComponent() {
 
             <input value={quantity} onChange={handleQuantityChange} type="number"/>
             <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange} placeholder="Enter deliver instructions"/>
+            <p>Comment: {comment}</p>
         </div>
     );
 }
