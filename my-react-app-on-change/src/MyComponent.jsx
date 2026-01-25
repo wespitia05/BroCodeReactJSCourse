d44dec4e2585ad1d759b0cbfbbb079a51b5cb46e
william <espiw@farmingdale.edu>
2026-01-25 13:22:15 -0500

    Bro Code reactjs onChange inputting a number

--- my-react-app-on-change/src/MyComponent.jsx
+++ my-react-app-on-change/src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent() {
     // creating stateful name variable w/ setter function
     // inital value is an empty string
     const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(0);
 
     // this function will handle changing the name
     // invoke set name function, access the event object, access target and get value property
@@ -11,11 +12,18 @@ function MyComponent() {
         setName(event.target.value);
     }
 
+    function handleQuantityChange(event) {
+        setQuantity(event.target.value);
+    }
+
     // onChange event handler triggers the function every time the input value changes
     return(
         <div>
             <input value={name} onChange={handleNameChange}/>
             <p>Name: {name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type="number"/>
+            <p>Quantity: {quantity}</p>
         </div>
     );
 }
