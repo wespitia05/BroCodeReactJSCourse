1cc1e721da79b2735c50d01add1456262ef8c50c
william <espiw@farmingdale.edu>
2026-01-26 14:14:50 -0500

    Bro Code reactjs updater function using arrow functions to update increment function

--- my-react-app-updater-functions/src/MyComponent.jsx
+++ my-react-app-updater-functions/src/MyComponent.jsx
@@ -6,7 +6,21 @@ function MyComponent() {
 
     // when this function is called, we increment the count by 1
     const increment = () => {
-        setCount(count + 1);
+        // uses the CURRENT state to calculate the NEXT state
+        // set functions do not trigger an update
+        // react batches together state updates for performance reasons
+        // NEXT state becomes the CURRENT state after an update
+        // setCount(count + 1); // 0 + 1 = 1  ; count doesn't update
+        // setCount(count + 1); // 0 + 1 = 1  ; count doesn't update
+        // setCount(count + 1); // 0 + 1 = 1  ; count doesn't update
+
+        // use arrow function to increment 3 times and rename count to previous count
+        // takes the PENDING state to calculate NEXT state
+        // react puts your updater function in a queue (waiting in line)
+        // during the next render, it will call them in the same order
+        setCount(prevCount => prevCount + 1); // 0 + 1 = 1
+        setCount(prevCount => prevCount + 1); // 1 + 1 = 2
+        setCount(prevCount => prevCount + 1); // 2 + 1 = 3
     }
     // when this function is called, we decrement the count by 1
     const decrement = () => {
@@ -19,7 +33,7 @@ function MyComponent() {
 
     return(
         <div className="counter-container">
-            <p className="count-display">{count}</p>
+            <p className="count-display">Count: {count}</p>
             <button className="counter-button" onClick={decrement}>Decrement</button>
             <button className="counter-button" onClick={reset}>Reset</button>
             <button className="counter-button" onClick={increment}>Increment</button>
