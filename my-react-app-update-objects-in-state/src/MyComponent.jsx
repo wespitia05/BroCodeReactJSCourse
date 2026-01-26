2b4c83ff0f9730366eded7087ea0ce86e5da5425
william <espiw@farmingdale.edu>
2026-01-26 15:32:19 -0500

    Bro Code reactjs update objects setting up our functions and assigning them to the html elements

--- my-react-app-update-objects-in-state/src/MyComponent.jsx
+++ my-react-app-update-objects-in-state/src/MyComponent.jsx
@@ -4,9 +4,24 @@ function MyComponent() {
     const [car, setCar] = useState({year: 2024, 
                                     make: "Ford", 
                                     model: "Mustang"});
+    
+    function handleYearChange(event) {
+
+    }
+    function handleMakeChange(event) {
+        
+    }
+    function handleModelChange(event) {
+        
+    }
+
     return(
         <div>
             <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+            
+            <input type="number" value={car.year} onChange={handleYearChange}/><br></br>
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br></br>
+            <input type="text" value={car.model} onChange={handleModelChange}/><br></br>
         </div>
     );
 }
