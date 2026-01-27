4bd3416c2bc771700a9fc187869a9e297556dae5
william <espiw@farmingdale.edu>
2026-01-27 18:18:56 -0500

    Bro Code reactjs update arrays displaying array

--- my-react-app-update-arrays/src/MyComponent.jsx
+++ my-react-app-update-arrays/src/MyComponent.jsx
@@ -1,8 +1,27 @@
 import React, {useState} from "react";
 
 function MyComponent() {
+    // array destructuring to create array of foods with setter function
+    // useState will set initial state to the following array
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+
+    function handleAddFood() {
+
+    }
+    function handleRemoveFood() {
+
+    }
+
+    // take array of foods, use map to return a new array
+    // add key to each list item to keep track, then within each list
+    // item return each food element
     return(
-        <p>hello</p>
+        <div>
+            <h2>List of Food</h2>
+            <ul>
+                {foods.map((food, index) => <li key={index}>{food}</li>)}
+            </ul>
+        </div>
     );
 }
 
