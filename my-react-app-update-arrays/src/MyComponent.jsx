40ed47c3d14e1267be386d0f9fb3e89e947c1a7a
william <espiw@farmingdale.edu>
2026-01-28 18:42:11 -0500

    Bro Code reactjs update arrays finished the remove food function

--- my-react-app-update-arrays/src/MyComponent.jsx
+++ my-react-app-update-arrays/src/MyComponent.jsx
@@ -15,18 +15,21 @@ function MyComponent() {
         // use spread operator to keep original array and add new food on top
         setFoods(prevFoods => [...prevFoods, newFood]);
     }
-    function handleRemoveFood() {
-
+    function handleRemoveFood(index) {
+        // using underscore for a parameter is a convention to indiciate that
+        // the paramter being passed to the function is being ignored 
+        setFoods(foods.filter((_, i) => i !== index));
     }
 
     // take array of foods, use map to return a new array
     // add key to each list item to keep track, then within each list
     // item return each food element
+    // since remove food has a parameter, we need to use an arrow function
     return(
         <div>
             <h2>List of Food</h2>
             <ul>
-                {foods.map((food, index) => <li key={index}>{food}</li>)}
+                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
             </ul>
             <input type="text" id="foodInput" placeholder="Enter Food Name"/>
             <button onClick={handleAddFood}>Add Food</button>
