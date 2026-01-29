25855b69e62accac7da6746975379f1f7abe30f8
william <espiw@farmingdale.edu>
2026-01-29 18:19:05 -0500

    Bro Code reactjs update array of objects created our constants

--- my-react-app-update-array-objects/src/MyComponent.jsx
+++ my-react-app-update-array-objects/src/MyComponent.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
 
 function MyComponent() {
+    const [cars, setCars] = useState([]);
+    const [carYears, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
+
     return(
         <p>hello</p>
     );
