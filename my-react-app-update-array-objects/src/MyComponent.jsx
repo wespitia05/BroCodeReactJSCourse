edbbaa231b66a4fda8702228f7ef79b4acf5f025
william <espiw@farmingdale.edu>
2026-01-30 14:47:05 -0500

    Bro Code reactjs update array of objects remove car function complete

--- my-react-app-update-array-objects/src/MyComponent.jsx
+++ my-react-app-update-array-objects/src/MyComponent.jsx
@@ -6,6 +6,7 @@ function MyComponent() {
     const [carMake, setCarMake] = useState("");
     const [carModel, setCarModel] = useState("");
 
+    // this function handles adding a new car
     function handleAddCar() {
         // create new constant to hold the new car information
         const newCar = {year: carYear, make: carMake, model: carModel};
@@ -19,8 +20,10 @@ function MyComponent() {
         setCarMake("");
         setCarModel("");
     }
+    // this function handles removing a car
+    // our parameter is the index from the list which will be removed after clicking on it
     function handleRemoveCar(index) {
-
+        setCars(prevCars => prevCars.filter((_, i) => i !== index));
     }
     // this function handles changing the year of the car
     // our parameter is an event bc we take the value of the input
@@ -43,7 +46,7 @@ function MyComponent() {
             <h2>List of Car Objects</h2>
             <ul>
                 {cars.map((car, index) => 
-                    <li key={index}>
+                    <li key={index} onClick={() => handleRemoveCar(index)}>
                         {car.year} {car.make} {car.model}
                     </li>)}
             </ul>
