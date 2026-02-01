3d938fac219151e07fe7fb70365ff5d0f931cdd8
william <espiw@farmingdale.edu>
2026-02-01 12:12:40 -0500

    Bro Code reactjs useEffect() some notes

--- my-react-app-use-effect-hook/src/MyComponent.jsx
+++ my-react-app-use-effect-hook/src/MyComponent.jsx
@@ -1,3 +1,21 @@
+// useEffect() = react hook that tells react "do some code when" (pick one):
+//               this component re-renders
+//               this component mounts
+//               the state of a value
+
+// useEffect(function, [dependencies])
+
+// 1. useEffect(() => {})           // runs after every re-render
+// 2. useEffect(() => {}, [])       // runs only on mount
+// 3. useEffect(() => {}, [value])  // runs on mount + when value changes
+
+// USES:
+// #1 Event Listeners
+// #2 DOM Manipulation
+// #3 Subscriptions (real-time updates)
+// #4 Fetching Data from an API
+// #5 Clean up when a component unmounts
+
 import React, {useState} from "react";
 
 function MyComponent() {
