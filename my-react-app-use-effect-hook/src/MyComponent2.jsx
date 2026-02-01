cf0ccc956ff0ecaf9c0fe7e3bcb71e644db4ed6b
william <espiw@farmingdale.edu>
2026-02-01 12:51:00 -0500

    Bro Code reactjs useEffect() second example

--- my-react-app-use-effect-hook/src/MyComponent2.jsx
+++ my-react-app-use-effect-hook/src/MyComponent2.jsx
@@ -2,8 +2,34 @@ import React, {useState, useEffect} from "react";
 
 // SECOND EXAMPLE, PLAYING WITH HEIGHT AND WIDTH
 function MyComponent2() {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED");
+
+        // do this code before next re-render or when component unmounts
+        return() => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED")
+        }
+    }, []);
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
+
+    function handleResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
     return(
-        <p>hello</p>
+        <div>
+            <p>Window Width: {width}px</p>
+            <p>Window Height: {height}px</p>
+        </div>
     );
 }
 
