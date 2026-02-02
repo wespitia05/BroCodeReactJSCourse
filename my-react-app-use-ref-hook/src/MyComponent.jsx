3c30d2dc1d0d2fc29c567cb73b2dba8bc61c5989
william <espiw@farmingdale.edu>
2026-02-01 23:37:48 -0500

    Bro Code reactjs useRef() example

--- my-react-app-use-ref-hook/src/MyComponent.jsx
+++ my-react-app-use-ref-hook/src/MyComponent.jsx
@@ -8,12 +8,48 @@
 //              2. handling focus, animations, and transitions
 //              3. managing timers and intervals
 
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function MyComponent() {
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log("COMPONENT RENDERED");
+    });
+
+    function handleClick1() {
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "Yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick2() {
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "Red";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick3() {
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "Blue";
+    }
+
     return(
         <div>
-            <p>hello</p>
+            <button onClick={handleClick1}>Click Me 1!</button>   
+            <input ref={inputRef1}/><br/> 
+
+            <button onClick={handleClick2}>Click Me 2!</button>   
+            <input ref={inputRef2}/> <br/>
+
+            <button onClick={handleClick3}>Click Me 3!</button>   
+            <input ref={inputRef3}/> 
         </div>
     );
 }
